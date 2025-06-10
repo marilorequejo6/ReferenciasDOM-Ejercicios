bfb4e08950b9ec040a1eebf8cee989d01061015e
marilorequejo6 <mreqjor.genius@gmail.com>
2025-06-10 13:04:32 +0200

    4 JS DOM Ejercicios insercion y borrado

--- public/javascript/functions/references.js
+++ public/javascript/functions/references.js
@@ -1,9 +1,16 @@
 // Creamos las referencias de todos los elementos
 
+const refParrafo1 = document.querySelector("#parrafo1");
+
 // Ejercicio 1:
 const refParrafo2 = document.createElement("p");
 
+// Ejercicio 2:
+const refParrafo3 = document.createElement("p");
+
 export { 
-    refParrafo2
+    refParrafo1,
+    refParrafo2,
+    refParrafo3
 };
 // Exportamos las referencias para poder usarlas en otros archivos
--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,8 +1,21 @@
 import {
-    refParrafo2
+    refParrafo1,
+    refParrafo2,
+    refParrafo3
 } from "/javascript/functions/references.js";
 
 // Ejercicio 1: 
-refParrafo2.className ="parrafo2";
+refParrafo2.className ="parrafo-añadido";
 refParrafo2.textContent = "Este es el segundo párrafo";
-refParrafo2.before(parrafo1)
+// Añadimos el segundo párrafo al body
+document.body.appendChild(refParrafo2);
+// Situamos el segundo párrafo antes del primer párrafo
+refParrafo1.before(refParrafo2);
+
+// Ejercicio 2:
+refParrafo3.className ="parrafo-añadido";
+refParrafo3.textContent = "Este es el tercer párrafo";
+// Añadimos el tercer párrafo al body
+document.body.appendChild(refParrafo3);
+// Situamos el tercer párrafo después del primer párrafo
+refParrafo1.after(refParrafo3);
