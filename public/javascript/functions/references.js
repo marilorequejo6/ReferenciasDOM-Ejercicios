ab64724438ef396a5277e718dbcb79e532533488
marilorequejo6 <mreqjor.genius@gmail.com>
2025-06-10 10:38:08 +0200

    3 JS DOM Ejercicios botones y eventos

--- public/javascript/functions/references.js
+++ public/javascript/functions/references.js
@@ -1,10 +1,4 @@
 // Creamos las referencias de todos los elementos
-/* const refParrafo1 = document.querySelector("#parrafo1");
-const refDiv1 = document.querySelector("#div1");
-const refLista1 = document.querySelector("#lista1");
-const refTabla1 = document.querySelector("#tabla1");
-const refColumna1 = document.querySelector("#columna1");
-const refPersona = document.querySelector("#persona"); */
 
 //Ejercicio 1:
 const refBoton1 = document.querySelector("#boton1");
--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,30 +1,4 @@
 import {refBoton1, refParrafo1, refBoton2, refLista1, refBoton3, refElemento1, refElemento3, refLista2, refLista3, refImgsLista3} from "/javascript/functions/references.js";
-/*
-// Ejercicio1: 
-console.log("Ejercicio1: ", refDiv1.previousElementSibling.textContent);
-
-// Ejercicio2:
-console.log("Ejercicio2, elemento 1: ", refLista1.children[0].textContent);
-console.log("Ejercicio2, elemento 2: ", refLista1.children[1].textContent);
-console.log("Ejercicio2, elemento 3: ", refLista1.children[2].textContent);
-
-// Ejercicio3:
-console.log("Ejercicio3: ", refLista1.previousElementSibling.previousElementSibling.textContent);   
-
-// Ejercicio4a:
-console.log("Ejercicio4a: ", refLista1.nextElementSibling.nextElementSibling.textContent);
-// Ejercicio4b:
-console.log("Ejercicio4b: ", refLista1.nextElementSibling.nextElementSibling.querySelector("#persona").textContent);
-
-// Ejercicio5:
-console.log("Ejercicio5: ", refTabla1.previousElementSibling.previousElementSibling.previousElementSibling.textContent);
-
-// Ejercicio6:
-console.log("Ejercicio6: ", refTabla1.querySelector("#persona").textContent);
-
-// Ejercicio7:
-console.log("Ejercicio7: ", refDiv1.nextElementSibling.nextElementSibling.nextElementSibling.textContent);
-*/
 
 // Ejercicio 1: Generar un texto al pulsar el botón
 refBoton1.addEventListener("click", () => {
