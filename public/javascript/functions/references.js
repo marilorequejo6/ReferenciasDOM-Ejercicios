391da96de03db17111974e75b8accb4cab8b172b
marilorequejo6 <mreqjor.genius@gmail.com>
2025-06-10 11:27:46 +0200

    4 JS DOM Ejercicios inserción y borrado

--- public/javascript/functions/references.js
+++ public/javascript/functions/references.js
@@ -1,32 +1,9 @@
 // Creamos las referencias de todos los elementos
 
-//Ejercicio 1:
-const refBoton1 = document.querySelector("#boton1");
+// Ejercicio 1:
+const refParrafo2 = document.createElement("p");
 
-// Ejercicio 2:
-const refParrafo1 = document.querySelector("#parrafo1");
-const refBoton2 = document.querySelector("#boton2");
-
-// Ejercicio 3:
-const refLista1 = document.querySelector("#lista1");
-const refBoton3 = document.querySelector("#boton3");
-
-// Ejercicio 4:
-const refElemento1 = document.querySelector("#elemento1");
-
-// Ejercicio 5:
-const refElemento3 = document.querySelector("#elemento3");
-
-// Ejercicio 6:
-const refLista2 = document.querySelector("#lista2");
-
-// Ejercicio 7 y 8:
-const refLista3 = document.querySelector("#lista3");
-// Para seleccionar todas las imágenes del documento
-const refImgs = document.querySelectorAll("img");
-// Para seleccionar todas las imágenes de la lista 3
-const refImgsLista3 = document.querySelectorAll("#lista3 img");
-
-
-export {refBoton1, refParrafo1, refBoton2, refLista1, refBoton3, refElemento1, refElemento3, refLista2, refLista3, refImgsLista3};
+export { 
+    refParrafo2
+};
 // Exportamos las referencias para poder usarlas en otros archivos
--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,68 +1,8 @@
-import {refBoton1, refParrafo1, refBoton2, refLista1, refBoton3, refElemento1, refElemento3, refLista2, refLista3, refImgsLista3} from "/javascript/functions/references.js";
+import {
+    refParrafo2
+} from "/javascript/functions/references.js";
 
-// Ejercicio 1: Generar un texto al pulsar el botón
-refBoton1.addEventListener("click", () => {
-    // Creamos un elemento para introducir el mensaje
-    const refDiv1 = document.createElement("div");
-    refDiv1.innerHTML = "Buenas a todas";
-    refDiv1.className = "div-ejercicio-1"; 
-    // Añadimos el elemento con el mensaje al body
-    document.body.prepend(refDiv1);
-});
-
-// Ejercicio 2: 
-refBoton2.addEventListener("click", () => {
-    console.log("Etiqueta: ", refParrafo1);
-    console.log("Texto: ", refParrafo1.textContent);
-});
-
-// Ejercicio 3:
-refBoton3.addEventListener("click", () => {
-    console.log("Etiqueta: ", refLista1);
-    console.log("Texto: ", refLista1.textContent);
-});
-
-// Ejercicio 4:
-refElemento1.addEventListener("click", () => {
-    console.log("Lista: ", refLista1.textContent);
-    console.log("Texto elemento 1: ", refElemento1.textContent);
-});
-
-// Ejercicio 5:
-refElemento3.addEventListener("click", () => {
-    console.log("Lista: ", refLista1.textContent);
-    console.log("Texto elemento 3: ", refElemento3.textContent);
-});
-
-// Ejercicio 6:
-refLista2.addEventListener("click", (e) => {
-    const li = e.target;
-
-    // Si el elemento clicado es un <li> y no tiene hijos, añadimos un <span>
-    if (li.childElementCount === 0) {
-        li.appendChild(añadirSpan());
-   }
-});
-
-function añadirSpan() {
-    const span = document.createElement("span");
-    span.textContent = "Este es el span añadido";
-    return span;
-};
-
-// Ejercicio 7 y 8:
-refLista3.addEventListener("click", (e) => {
-    // Si el click es sobre una etiqueta img, mostramos un mensaje
-    // e.target.tagName nos da el nombre de la etiqueta del elemento clicado en mayúsculas
-    // por eso hay que utilizar toLowerCase() para convertirlo en minúsculas y compararlo con "img"
-    if (e.target.tagName.toLowerCase() === "img") {
-        console.log("Imagen clicada por el ejercicio 7");
-    }
-});
-
-// Recorremos todas las imágenes de la lista 3 y añadimos un evento click a cada una que muestre un mensaje
-refImgsLista3.forEach((img) => {
-    img.addEventListener("click", (e) => {
-        console.log("Imagen clicada por el ejercicio 8");
-    });
-});
+// Ejercicio 1: 
+refParrafo2.className ="parrafo2";
+refParrafo2.textContent = "Este es el segundo párrafo";
+refParrafo2.before(parrafo1)
